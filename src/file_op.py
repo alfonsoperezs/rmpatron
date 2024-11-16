96d140095e22950cb5b26bf87ca5ddde20f992ef
alfonsoperezs <fonpesan@gmail.com>
2024-11-16 13:59:30 +0100

    file_op.py refactoring. Fixing minor details.

--- src/file_op.py
+++ src/file_op.py
@@ -6,25 +6,25 @@ class File_op:
         self.__deleted = 0
         self.__deleted_path = []
     
-    def ex_file_op(self, option, args):
+    def execute_operation(self, option, args):
         self.__deleted = 0
         self.__deleted_path = []
         match option:
             case "-r":
-                return self.__option_r(args[0], args[1:])
+                return self.__recursive_delete_by_name(args[0], args[1:])
             
-    def __option_r(self, path, names_files):
+    def __recursive_delete_by_name(self, path, names_files):
         content = os.listdir(path)
         for x in content:
             abs_path = os.path.join(path,x)
             if x in names_files:
-                self.__remove_file(abs_path)
+                self.__remove_file_or_dir(abs_path)
             else:
                 if os.path.isdir(abs_path):
-                    self.__option_r(abs_path,names_files)
+                    self.__recursive_delete_by_name(abs_path,names_files)
         return self.__deleted, self.__deleted_path
                     
-    def __remove_file(self, path):
+    def __remove_file_or_dir(self, path):
         if os.path.isdir(path):
             shutil.rmtree(path)
             self.__deleted += 1
--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
-from command_reader import *
+from command_tool import *
 
 if __name__ == '__main__':
     run = True
-    command_reader = Command_reader() 
-    print("--------------------------- Welcome to RM PATRON ---------------------------")
-    print("----------------------------------------------------------------------------")
+    command_tool = Command_tool() 
     while run:
-        command = input("-> ")
-        run = command_reader.process_command(command)
+        command = input("[RMPATRON] -> ")
+        run = command_tool.process_command(command)
         
